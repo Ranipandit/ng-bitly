e97e411c8203412c35dffd0b6c5247fe0aab09a2
Akaash Patnaik <akaashanky@gmail.com>
2018-12-28 21:47:00 +0530

    Simple mongo save

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+const dbConnection = mongoose.connection
+dbConnection.on('open', () => {
+    console.log('Connected to DB!')
+})
+
+const PersonSchema = {
+    name: String
+}
+
+const Person = mongoose.model("Person", { name: String });
+
 const loggerMiddleware = (req, res, next) => {
     console.log(`${req.method} ${req.url}`)
     next()
@@ -16,9 +29,16 @@ app.get('/', (req, res) => {
 })
 
 app.post('/hello', (req, res) => {
-    return res.status(200).send({
-        message: `Hello ${req.body.name}`
+    const newPerson = new Person({
+        name: req.body.name
     })
+    newPerson.save()
+    .then(doc => {
+        return res
+          .status(200)
+          .send(doc);
+    })
+    
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
