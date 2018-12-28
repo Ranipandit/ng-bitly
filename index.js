9e70dd173eea9807f23dd46f8accd1e35d1a8adc
Akaash Patnaik <akaashanky@gmail.com>
2018-12-28 21:35:44 +0530

    JSON body parsing

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const loggerMiddleware = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`)
+    next()
+}
+
+
+app.use(loggerMiddleware)
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    return res.send('Hello World!')
+})
+
+app.post('/hello', (req, res) => {
+    return res.status(200).send({
+        message: `Hello ${req.body.name}`
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
