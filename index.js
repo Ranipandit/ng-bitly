ae69918c337cfb3c571d6fd3e4e0f9a19d36c86e
Ranipandit <rani17@navgurukul.org>
2019-01-05 11:54:57 +0800

    testing done

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 const shortid = require('shortid')
+
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
@@ -26,33 +27,40 @@ app.use(loggerMiddleware)
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    return res.send('Hello World!')
+    return res.send('Hello World')
 })
 
 app.post('/shorten', (req, res) => {
+    console.log(req.body)
     URL.findOne({url: req.body.url}).exec()
-    .then(existingUrl => {
-       if (existingUrl) {
-            return existingUrl;
-       } else {
-            const hash = shortid.generate();
-            return URL.create({ hash: hash, url: req.body.url });
-       }
-    }).then(doc => {
-        return res.status(201).send(doc)
-    })
+        .then(existingUrl => {
+            if (existingUrl) {
+                    return existingUrl;
+            } else {
+                    const hash = shortid.generate();
+                    return URL.create({ hash: hash, url: req.body.url });
+            }
+        })
+        .then(doc => {
+            return res.status(201).send(doc)
+        })
 })
 
 app.get('/:hash', (req, res) => {
+    console.log(req.params);
     URL.findOne({hash: req.params.hash}).exec()
-    .then(existingUrl => {
-        if (existingUrl) {
-            console.log("Redirecting...")
-            return res.redirect(existingUrl.url);
-        } else {
-            return res.status(404)
-        }
-    })
+        .then(existingUrl => {
+            if (existingUrl) {
+                console.log("Redirecting...")
+                return URL.update({hash: req.params.hash},{$set:{hits: existingUrl.hits+1}}).exec()
+                            .then(() => {
+                                console.log(existingUrl)
+                                return res.redirect(existingUrl.url);
+                            })
+            } else {
+                return res.status(404)
+            }
+        })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- test.js
+++ test.js
@@ -2,24 +2,24 @@ const expect = require('chai').expect
 const axios = require('axios')
 
 describe("API Tests", function() {
-  let id, hash;
+  let id, hash,hits;
   describe("/shorten", function() {
     
     it("should return a shortened URL", (done) => {
         const request = { url: "https://google.com" }
-        axios.post('http://localhost:3000/shorten', request)
-        .then(response => {
-            id = response.data._id
-            hash = response.data.hash
-            expect(response.status).to.equal(201)
-            expect(response.data._id).to.be.ok
-            expect(response.data.hash).to.be.ok;
-            done()
-        })
+        axios.post('http://localhost:5000/shorten', request)
+          .then(response => {
+              id = response.data._id
+              hash = response.data.hash
+              expect(response.status).to.equal(201)
+              expect(response.data._id).to.be.ok
+              expect(response.data.hash).to.be.ok;
+              done()
+          })
     });
     it("should return the same shortened URL for the earlier url", (done) => {
         const request = { url: "https://google.com" }
-        axios.post('http://localhost:3000/shorten', request)
+        axios.post('http://localhost:5000/shorten', request)
         .then(response => {
             expect(response.status).to.equal(201)
             expect(response.data._id).to.equal(id)
@@ -29,12 +29,22 @@ describe("API Tests", function() {
     });
   });
 
-  describe("/:hash", function() {
+  describe("/shorten/:hash", function() {
 
     it("should redirect the user for an existing hash", (done) => {
-      axios.get("http://localhost:3000/"+hash)
+      axios.get("http://localhost:5000/shorten/"+hash)
+        .then(response => {
+          expect(response.data).to.contain("google.com")
+          done()
+        })
+    })
+  })
+
+  describe("/:hash", function(){
+    it ("should count the hash",(done) => {
+      axios.post("http://localhost:5000/"+hash)
       .then(response => {
-        expect(response.data).to.contain("google.com")
+        expect(response.data.hits).to.be.ok
         done()
       })
     })
