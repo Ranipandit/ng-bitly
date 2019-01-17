cdafe7b015ef83f12d34acfb6782e1c5b81c08ef
Ranipandit <rani17@navgurukul.org>
2019-01-17 12:24:57 +0800

    stucked in an error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 5000
 const shortid = require('shortid')
 
-
+// require mongoose to connect nodejs and mongodb
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
 const dbConnection = mongoose.connection
@@ -11,25 +11,32 @@ dbConnection.on('open', () => {
     console.log('Connected to DB!')
 })
 
+// schema of the url
 const URL = mongoose.model("url", {
     hash: String,
     url: String,
     hits: { type: Number, default: 0 }
 });
 
+// middlewares to connect the request and url
 const loggerMiddleware = (req, res, next) => {
     console.log(`${req.method} ${req.url}`)
     next()
 }
 
-
+// used the middlewares in the app
 app.use(loggerMiddleware)
+// the data should be contain in json
 app.use(express.json())
 
+// this is the default home route which wil send only 'hello world'
 app.get('/', (req, res) => {
     return res.send('Hello World')
 })
 
+// this is the shorten endpoint(/shorten) where we will send the url to generate a hash 
+// if the url is exist already so it will not short the url otherwise if the url is 
+// new than it will generate new hash url
 app.post('/shorten', (req, res) => {
     console.log(req.body)
     URL.findOne({url: req.body.url}).exec()
@@ -46,6 +53,10 @@ app.post('/shorten', (req, res) => {
         })
 })
 
+
+// created hit endpoint(/hits)
+// it will query the hits and then it will give the result of the number of times
+// we used the hash url url
 app.get('/hits',(req, res) => {
     console.log('***', req.query.hash);
     URL.findOne({hash: req.query.hash}).exec()
@@ -60,20 +71,27 @@ app.get('/hits',(req, res) => {
 
 })
 
+// hash endpoint(/:hash)
+// here we will the pass the hash url as a parameter it will check if the url is existing
+// than it will redirect to url and also update the hits otherwise it will throw an error of (404) not found
+
 app.get('/:hash', (req, res) => {
     console.log('&&&', req.params);
-    URL.findOne({hash: req.params.hash}).exec()
+    URL.findOne({hash: req.params.hash,url:req.body.url,hits:req.body.hits}).exec()
         .then(existingUrl => {
             console.log(existingUrl)
-            if (existingUrl) {
+            if (existingUrl.hits<6) {
                 console.log("Redirecting...")
                 return URL.update({hash: req.params.hash},{$set:{hits: existingUrl.hits+1}}).exec()
                             .then(() => {
                                 console.log(existingUrl)
+
                                 return res.redirect(existingUrl.url);
                             })
             } else {
-                return res.sendStatus(404)
+                const hash = shortid.generate();
+                const status = res.sendStatus(404)
+                return URL.create({ hash: hash, url: req.body.url,status:status});
             }
         })
 })
